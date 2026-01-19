d551b4d84af90be2e766c600c3a30439fa35a32f
Martin Kibera <martin.kibera.n@gmail.com>
2026-01-19 16:07:55 +0300

    add music videos support

--- api/video.js
+++ api/video.js
@@ -123,8 +123,17 @@ module.exports = [
 
 			const instanceOrigin = settings.local === 1 ? "http://localhost:3000" : settings.instance;
 			const videoFuture = req.method === "GET"
-				? request(`${instanceOrigin}/api/v1/videos/${id}`).then(res => res.json())
-				: JSON.parse(new URLSearchParams(body.toString()).get("video"));
+				? (async () => {
+					// Fetch with invidious proxy if Music genre is found
+					let v = await request(`${instanceOrigin}/api/v1/videos/${id}`).then(res => res.json());
+
+					if (v && v.genre === "Music") {
+						v = await request(`${instanceOrigin}/api/v1/videos/${id}?local=1`).then(res => res.json());
+					}
+
+					return v;
+				})()
+				: Promise.resolve(JSON.parse(new URLSearchParams(body.toString()).get("video")));
 
 			const commentsFuture = request(`${instanceOrigin}/api/v1/comments/${id}`)
 				.then(res => res.json())
